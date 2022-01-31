995497810436ce0ec8f01485cc160e60d65fb997
DucksIncoming <ducksincoming@gmail.com>
2022-01-31 03:22:29 -0500

    Navbar full style

--- js/home.js
+++ js/home.js
@@ -1,9 +1,20 @@
 // Animation handling
 
+navbar = document.getElementById("navbar-container");
+navIcon = document.getElementById("navbar-icon");
+
 function navbarOpen() {
-    
+    navbar.style.transitionDuration = "0.25s";
+    navbar.style.transform = "translateX(0%)";
+
+    navIcon.style.transitionDuration = "0.25s";
+    navIcon.style.transform = "rotate(90deg)";
 }
 
 function navbarClose() {
-    
+    navbar.style.transitionDuration = "0.25s";
+    navbar.style.transform = "translateX(-100%)";
+
+    navIcon.style.transitionDuration = "0.25s";
+    navIcon.style.transform = "rotate(0deg)";
 }
