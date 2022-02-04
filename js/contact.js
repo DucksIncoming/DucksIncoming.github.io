f950151251740935a4e542acc812aaf0d9d08be6
DucksIncoming <ducksincoming@gmail.com>
2022-02-03 20:37:50 -0500

    contact stuff

--- js/contact.js
+++ js/contact.js
@@ -6,11 +6,22 @@ sendButton = document.getElementById("send-button")
 confirmBanner = document.getElementById("confirm-banner");
 form = document.getElementById("contact-form");
 
+function handleForm(event) { 
+    event.preventDefault(); 
+}
+
 function send() {
+    form.addEventListener('submit', handleForm);
     
     confirmBanner.style.opacity = "1";
+    confirmBanner.transform = "translateY(0%)";
+    name.value = "";
+    email.value = "";
+    org.value = "";
+    msg.value = "";
 }
 
 function closeBanner() {
     confirmBanner.style.opacity = "0";
+    confirmBanner.transform = "translateY(100%)";
 }
